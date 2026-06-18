e6d97f631c829a9728acd9db42e9a20058e3dfa4
Tal Solovey <tal.s@fellowship.masterschool.com>
2026-06-18 15:52:50 +0300

    users: replace in-memory store with mongo dao.

--- backend-nest/src/modules/users/users.module.ts
+++ backend-nest/src/modules/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
+import { MongoUsersRepository } from './persistence/mongo-users.repository';
+import { UserSchema } from './persistence/user.schema';
 
 @Module({
-  providers: [UsersService, UsersRepository],
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  providers: [
+    UsersService,
+    { provide: UsersRepository, useClass: MongoUsersRepository },
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
