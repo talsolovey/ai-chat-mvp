b6cb740dc1169b9f51723087e37a25f075ec108a
Tal Solovey <tal.s@fellowship.masterschool.com>
2026-07-05 17:15:30 +0300

    backend-nest: fix lint formatting and remove redundant cast.

--- backend-nest/src/modules/messages/messages.controller.ts
+++ backend-nest/src/modules/messages/messages.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Query,
-  Res,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Res } from '@nestjs/common';
 import type { Response } from 'express';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import type { PublicUser } from '../users/user.entity';
